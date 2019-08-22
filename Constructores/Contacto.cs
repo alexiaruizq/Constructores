1036aaeb4094077322a54c6d7e37e1a636e2358a
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-22 07:31:46 -0700

    Agregar archivos de proyecto.

---
+++ Constructores/Contacto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructores
+{
+    class Contacto
+    {
+        public string Nombre { get; set; }
+        public string Celular { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+
+        //Los constructores siempre llevan el nombre de la clase
+        public Contacto()
+        {
+            Nombre = "No name";
+            Celular = "No Cellphone";
+            Telefono = "Unaviable";
+            Correo = "Not found";
+            Direccion = "Unknown";
+
+        }
+
+        //Contructor de parametros 
+        public Contacto(string nombre, string correo)
+        {
+            //Variable parametro
+            Nombre = nombre;
+            Correo = correo;
+            Celular = "No cellphone";
+            Direccion = "Unkown";
+            Telefono = "Unaviable";
+
+        }
+        //no se puede hacer otro constructor que tenga dos strings porque ya esta arriba, tienen que ser mas
+        public Contacto(string nombre, string correo, string celular, string direccion, string telefono);
+        
+}
+}
+
---
+++ Constructores/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //para Constructor 1
+            Contacto contacto1 = new Contacto();
+
+            contacto.1 Nombre = "Alexia";
+            contacto.1 Celular = "(644) 1323465";
+            contacto.1 Correo = "alexiaruiz-@hotmail.com";
+            //Fin del constructor
+
+            Console.WriteLine("Contacto 1: ");
+            Console.WriteLine("Nombre: " + contacto1.Nombre);
+            Console.WriteLine("Celular:" + contacto1.Celular );
+            Console.WriteLine("Correo: " + contacto1.Correo);
+
+            //Para constructor 2
+            Contacto contacto2 = new Contacto("Alexia", "alexiaruiz-@hotmail.com");
+            //Fin del constructor 2
+
+            Console.WriteLine("Contacto 2: ");
+            Console.WriteLine("Nombre: " + contacto2.Nombre);
+            Console.WriteLine("Celular:" + contacto2.Celular);
+            Console.WriteLine("Correo: " + contacto2.Correo);
+
+            Console.Read();
+        }
+    }
+}
